a8fb37bfb4cd5732041160c429a56fa1fa92a1db
karolw881 <karolw881@gmail.com>
2026-09-15 18:32:02 +0200

    Nauka routingu

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'MAIN PAGE ';
+});
+
+Route::get('/xxx', function () {
+    return 'hello';
+})->name('hello');
+
+Route::get("/greet/{name}", function ($name) {
+    return "Hello, $name!";
+});
+
+Route::get('/hallo', function () {
+    return  redirect()->route('hello');
+});
+
+Route::fallback(function () {
+    return 'Still here? You must have taken a wrong turn.';
 });
